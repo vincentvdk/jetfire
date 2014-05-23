03779e4f4283b999289a8f0727864590d3767aae
Vincent Van der Kussen <vincent@vanderkussen.org>
2014-05-23 16:04:27 +0200

    changed structure a bit for easier deployment on Apache

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,72 @@
 #
 
 import flask
+import flask.views
 
-app = flask.Flask(__name__)
-app.config.from_pyfile('config.cfg')
+
+app = flask.Flask('app')
+app.config.from_pyfile('../config.cfg')
+
+from add_host import AddHost
+from get_host import GetHost, GetAllHosts
+from get_group import GetGroup, GetAllGroups
+from add_group import AddGroup
+from edit_host import EditHost, EditHostSubmit
+from edit_group import EditGroup, EditGroupSubmit
+#from remove import RemoveGroup, RemoveHost
+from remove import Remove
+
+class Main(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+
+    def post(self):
+        pass
+
+app.add_url_rule('/',
+                view_func=Main.as_view('index'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/addhost',
+                view_func=AddHost.as_view('addhost'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/gethostinfo',
+                view_func=GetHost.as_view('gethostinfo'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/getallhosts',
+                view_func=GetAllHosts.as_view('getallhosts'),
+                methods=['POST','GET'])
+
+app.add_url_rule('/edithost',
+                view_func=EditHost.as_view('edithost'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/edithostsubmit',
+                view_func=EditHostSubmit.as_view('edithostsubmit'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/editgroup',
+                view_func=EditGroup.as_view('editgroup'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/editgroupsubmit',
+                view_func=EditGroupSubmit.as_view('editgroupsubmit'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/getgroup',
+                view_func=GetGroup.as_view('getgroup'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/getallgroups',
+                view_func=GetAllGroups.as_view('getallgroups'),
+                methods=['POST','GET'])
+
+app.add_url_rule('/addgroups',
+                view_func=AddGroup.as_view('addgroup'),
+                methods=['GET', 'POST'])
+
+app.add_url_rule('/remove',
+                view_func=Remove.as_view('remove'),
+                methods=['GET', 'POST'])
---
+++ run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+from app import app
+
+app.run(host='0.0.0.0')
+
