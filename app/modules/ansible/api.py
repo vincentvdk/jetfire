a45e5e9c54b7eabe91940d8aafe8b9b85dfa18f1
Vincent Van der Kussen <vincent@vanderkussen.org>
2014-10-20 21:50:18 +0200

    pep8 cleanup

--- app/modules/ansible/api.py
+++ app/modules/ansible/api.py
@@ -1,26 +1,29 @@
 from flask.ext.restful import Resource
 from flask import request, jsonify
-from app import app,common
+from app import app
 from subprocess import Popen
 from os import listdir
-import json, os
+import os
 
 playdir = os.getenv("PLAYBOOK_DIR", app.config['PLAYBOOK_DIR'])
 inventory = os.getenv("ANSIBLE_INVENTORY", app.config['ANSIBLE_INVENTORY'])
 
-class ansibleAPI(Resource):
-	def get(self):
-		playbooks = {"playbooks": [p for p in listdir(playdir) if p.endswith(".yml")]}
-		resp = jsonify(playbooks)
-		resp.status_code = 200
-		return resp
 
-	def post(self):
-		data = request.json
-		process = Popen("ansible-playbook -i " + inventory + " " + data['play'] + " -u " + data['user'], shell=True)
-		process.communicate()
-		exitcode = process.wait()
-		if exitcode == 0:
-			return 'playbook %s successfully completed' % data['play'], 201
-		elif exitcode == 1:
-			return 'playbook %s failed' % data['play'], 201
+class ansibleAPI(Resource):
+    def get(self):
+        playbooks = {"playbooks": [p for p in listdir(playdir) if
+                     p.endswith(".yml")]}
+        resp = jsonify(playbooks)
+        resp.status_code = 200
+        return resp
+
+    def post(self):
+        data = request.json
+        process = Popen("ansible-playbook -i " + inventory + " "
+                        + data['play'] + " -u " + data['user'], shell=True)
+        process.communicate()
+        exitcode = process.wait()
+        if exitcode == 0:
+            return 'playbook %s successfully completed' % data['play'], 201
+        elif exitcode == 1:
+            return 'playbook %s failed' % data['play'], 201
