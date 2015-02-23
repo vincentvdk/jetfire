f015ad0bd4d0be8f3ba818202a64c05654ec0bc4
Vincent Van der Kussen <vincent@vanderkussen.org>
2015-02-23 13:43:35 +0100

    adjusted click in addhost so post runs only when button is clicked

--- app/static/backbone_jetfire.js
+++ app/static/backbone_jetfire.js
@@ -1,3 +1,4 @@
+// Collection and models
 
 var Hosts = Backbone.Collection.extend({
   url: '/api/v1.0/hosts'
@@ -33,21 +34,27 @@ var Host = Backbone.Model.extend({
   }
 });
 
-var Groups = Backbone.Collection.extend({
-  url: '/api/v1.0/groups'
+var DeleteHost = Backbone.Model.extend({
+  urlRoot: '/api/v1.0/hosts',
+  //idAttribute: "id"
 });
+
+var Groups = Backbone.Collection.extend({
+  url: '/api/v1.0/groups',
+  idAttribute: "groups"
+});
+
 
 // Views
 var HostList = Backbone.View.extend ({
   el: '.page',
   render: function() {
-    var that = this;
+    var self = this;
     var hosts = new Hosts();
     hosts.fetch({
       success: function(hosts) {
         var template = _.template($('#host-list-template').html());
-        that.$el.html(template({hosts: hosts.models}));
-        //console.log(hosts);
+        self.$el.html(template({hosts: hosts.models}));
       }
     })
   }
@@ -60,7 +67,8 @@ var AddHost = Backbone.View.extend ({
     this.$el.html(template);
   },
   events: {
-    'submit .add-host-form': 'saveHost'
+    //'click .add-host-form': 'saveHost'
+    'click button#addhost-btn': 'saveHost'
   },
   saveHost: function(event) {
     var newhostmodel = new Host ({
@@ -75,12 +83,32 @@ var AddHost = Backbone.View.extend ({
   }
 });
 
-
+/*var RemoveHost = Backbone.View.extend ({
+  el: '.page',
+  events: {
+    'click .remove-host': 'removeHost'
+  },
+  removeHost: function(event) {
+    console.log('entered delete view host' + id);
+    //var hostremovemodel = new DeleteHost({id: 'test.philippe'});
+    var hostremovemodel = new DeleteHost(id);
+    console.log(hostremovemodel)
+    hostremovemodel.destroy();
+  }
+});
+*/
 var GroupList = Backbone.View.extend ({
   el: '.page',
+  //console.log('data from route to view' + groups)
   render: function() {
-    var template = _.template($('#group-list-template').html());
-    this.$el.html(template);
+    var self = this;
+    var groups = new Groups();
+    groups.fetch({
+      success: function(groups) {
+        var template = _.template($('#group-list-template').html());
+        self.$el.html(template({groups: groups.models}));
+      }
+    })
   }
 });
 
@@ -89,9 +117,9 @@ var GroupList = Backbone.View.extend ({
 var hostList = new HostList();
 var groupList = new GroupList();
 var addHost = new AddHost();
+//var removeHost = new RemoveHost();
 
-//# endviews
-
+// # endviews ####################
 
 // Routes
 var Router  = Backbone.Router.extend ({
@@ -100,7 +128,8 @@ var Router  = Backbone.Router.extend ({
     'hosts': 'hosts',
     'groups': 'groups',
     'addhost': 'addhost',
-    'hosts/:id/vars': 'hostvars'
+    'hosts/:id/vars': 'hostvars',
+    'hosts/:id': 'removehost'
   }
 });
 
@@ -115,11 +144,24 @@ router.on('route:hosts', function(){
 });
 router.on('route:groups', function(){
   console.log('we have loaded the groupslist page');
+  var groups = new Groups();
+  groups.fetch({
+    success: function(groups) {
+      groups = {groups: groups.models.toJSON}
+      console.log(groups)
+    }
+  })
+  //console.log("return groups in route: " + groups.fetch())
   groupList.render();
 });
 router.on('route:addhost', function(){
   console.log('we have loaded the addhost page');
   addHost.render();
+});
+router.on('route:removehost', function(id){
+  console.log('/api/v1.0/hosts/', id);
+  var hostremovemodel = new DeleteHost({id: id});
+  hostremovemodel.destroy();
 });
 
 //Start Backbone history a necessary step for bookmarkable URL's
